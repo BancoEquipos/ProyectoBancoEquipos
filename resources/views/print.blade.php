fed640f27a113f4c2fb09bd32bba0cd41ebe8d1a
Borja <borjaab1997@gmail.com>
2021-05-26 23:08:30 +0200

    Puesta en comun

---
+++ resources/views/print.blade.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>Document</title>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min.js"></script>
+    <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/jspdf/1.1.135/jspdf.min.js"></script>
+    <script type="text/javascript" src="print.js"></script>
+</head>
+    <body id="body">
+        <p>Imprimir pdf funciona con laravel</p>
+        <button id="boton">PDF</button>
+  </body>
+</html>
