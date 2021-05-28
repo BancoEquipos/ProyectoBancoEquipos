01608a0ac3eeaef02192f4c9ed3815740d011e69
Borja <borjaab1997@gmail.com>
2021-05-29 00:00:44 +0200

    Funcionando en local y con la migración hecha

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -1,9 +1,5 @@
 <?php
-
-
 namespace App\Http\Controllers\Auth;
-
-
 use App\Http\Controllers\Controller;
 use Laravel\Socialite\Facades\Socialite;
 use Illuminate\Support\Facades\Auth;
@@ -13,57 +9,35 @@ use App\Models\Team;
 
 class LoginController extends Controller
 {
-    /**
-     * Redirect the user to the GitHub authentication page.
-     *
-     * @return \Illuminate\Http\Response
-     */
+
     public function redirectToGoogle()
     {
         return Socialite::driver('google')->redirect();
     }
 
-    /**
-     * Obtain the user information from GitHub.
-     *
-     * @return \Illuminate\Contracts\Foundation\Application|\Illuminate\Http\RedirectResponse|\Illuminate\Http\Response|\Illuminate\Routing\Redirector
-     */
     public function handleGoogleCallback()
     {
         try {
-            //create a user using socialite driver google
+
             $user = Socialite::driver('google')->user();
-            // if the user exits, use that user and login
             $finduser = User::where('google_id', $user->id)->first();
-            if($finduser){
-                //if the user exists, login and show dashboard
+
+            if ($finduser) {
                 Auth::login($finduser);
-                return redirect('/dashboard');
-            }else{
-                //user is not yet created, so create first
+//                return dd($user->getEmail());
+                return redirect('/print');
+            } else {
                 $newUser = User::create([
                     'name' => $user->name,
                     'email' => $user->email,
                     'google_id'=> $user->id,
                     'password' => encrypt('')
                 ]);
-                //every user needs a team for dashboard/jetstream to work.
-                //create a personal team for the user
-                $newTeam = Team::forceCreate([
-                    'user_id' => $newUser->id,
-                    'name' => explode(' ', $user->name, 2)[0]."'s Team",
-                    'personal_team' => true,
-                ]);
-                // save the team and add the team to the user.
-                $newTeam->save();
-                $newUser->current_team_id = $newTeam->id;
-                $newUser->save();
-                //login as the new user
                 Auth::login($newUser);
-                // go to the dashboard
-                return redirect('/dashboard');
+//                return dd($user->getName());
+                return redirect('/email');
             }
-            //catch exceptions
+
         } catch (Exception $e) {
             dd($e->getMessage());
         }
---
+++ database/migrations/2021_05_28_213930_add_google_id_column.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class AddGoogleIdColumn extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        if (!Schema::hasColumn('users', 'google_id'))
+        {
+            Schema::table('users', function (Blueprint $table)
+            {
+                $table->text('google_id');
+            });
+        }
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        if (Schema::hasColumn('users', 'google_id'))
+        {
+            Schema::table('users', function (Blueprint $table)
+            {
+                $table->dropColumn('google_id');
+            });
+        }
+    }
+}
