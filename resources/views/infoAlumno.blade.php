fdaa7eb5515679f34cb25e37d415ca9be32d8cda
Borja <borjaab1997@gmail.com>
2021-06-01 19:12:15 +0200

    css con asset

--- resources/views/infoAlumno.blade.php
+++ resources/views/infoAlumno.blade.php
@@ -7,7 +7,7 @@
 
     <!-- Bootstrap CSS -->
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/css/bootstrap.min.css" integrity="sha384-B0vP5xmATw1+K9KRQjQERJvTumQW0nPEzvF6L/Z6nronJ3oUOFUFpCjEUQouq2+l" crossorigin="anonymous">
-    <link rel="stylesheet" href="/alumno/css/style.css">
+    <link rel="stylesheet" href="{{ asset('/alumno/css/style.css') }}">
 
     <title>Seccion Alumno</title>
     <link rel="icon" href="{{ asset('alumno/images/logoC3Mobile.svg') }}">
