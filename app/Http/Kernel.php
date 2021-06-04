479032b0f81e8667f9f38ed765f01c894574cb8a
Borja <borjaab1997@gmail.com>
2021-06-04 23:00:07 +0200

    Test 2 middleware

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -62,6 +62,6 @@ class Kernel extends HttpKernel
         'signed' => \Illuminate\Routing\Middleware\ValidateSignature::class,
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
         'verified' => \Illuminate\Auth\Middleware\EnsureEmailIsVerified::class,
-        'routes' => \Illuminate\Auth\Middleware\RedirectViews::class,
+        'routes' => \App\Http\Middleware\RedirectViews::class,
     ];
 }
