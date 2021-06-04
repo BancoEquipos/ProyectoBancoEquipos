b304f4dac4ddf9cab06c5908170e20becebf2bc6
Borja Aznar Berenguer <borja.aznar@dosfarma.com>
2021-06-04 13:07:43 +0200

    Hola

--- resources/views/alumno.blade.php
+++ resources/views/alumno.blade.php
@@ -8,5 +8,6 @@
 <h1>Apellido: {{ $surnames }}</h1>
 <h1>NRE: {{ $nre }}</h1>
 <a href="{{ url('vistaAlumno') }}">Ir a la sección de alumno</a>
+<a href="{{ url('auth/google') }}">¿No eres tu? Haz login en tu cuenta</a>
 </body>
 </html>
