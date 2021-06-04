858433db67c61268aebd25b37b454d4e1e42124a
Borja <borjaab1997@gmail.com>
2021-06-04 19:38:36 +0200

    Add loans tables

---
+++ database/migrations/2021_06_04_170803_create_loans_components_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateLoansComponentsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('loans_components', function (Blueprint $table) {
+            $table->id('loan_id');
+            $table->foreign('loan_id')->references('id')->on('loans');
+            $table->id();
+            $table->bigInteger('component_id');
+            $table->foreign('component_id')->references('id')->on('component_type');
+            $table->bigInteger('specific_component_id');
+            $table->foreign('specific_component_id')->references('id')->on('specific_component');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('loans_components');
+    }
+}
