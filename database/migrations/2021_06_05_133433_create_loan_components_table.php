028e17cb1aac78d9c50c01587a7d69fccd82b436
Borja <borjaab1997@gmail.com>
2021-06-05 16:34:41 +0200

    a

--- database/migrations/2021_06_05_133433_create_loan_components_table.php
+++ database/migrations/2021_06_05_133433_create_loan_components_table.php
@@ -14,18 +14,14 @@ class CreateLoanComponentsTable extends Migration
     public function up()
     {
         Schema::create('loan_components', function (Blueprint $table) {
-            $table->unsignedBigInteger('loan_id');
-            $table->unsignedBigInteger('id');
-            $table->primary(['loan_id', 'id']);
+            $table->id();
+            $table->bigInteger('load_id')->primary();
             $table->foreign('loan_id')->references('id')->on('loans');
             $table->bigInteger('component_id');
             $table->foreign('component_id')->references('id')->on('component_types');
             $table->bigInteger('specific_component_id');
             $table->foreign('specific_component_id')->references('id')->on('specific_components');
             $table->timestamps();
-        });
-        Schema::table('loan_components', function (Blueprint $table) {
-            $table->bigInteger('id', true, true)->change();
         });
     }
 
