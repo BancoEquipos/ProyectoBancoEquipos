ff807628d71a2c497c3a0bfcc5cad4d08b41482f
Borja <borjaab1997@gmail.com>
2021-06-05 19:52:01 +0200

    hola

--- app/Http/Middleware/RedirectViews.php
+++ app/Http/Middleware/RedirectViews.php
@@ -11,7 +11,7 @@ class RedirectViews
     public function handle(Request $request, Closure $next)
     {
         if (Auth::check()) {
-            return 'estas logeado';
+            return 'estas logeado' .$next($request);
         }
         return 'no estas logeado';
     }
