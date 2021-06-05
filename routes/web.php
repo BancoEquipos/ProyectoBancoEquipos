e3e25e108792b9c9d170b4c4cd7fb64e3691e26f
Borja <borjaab1997@gmail.com>
2021-06-05 20:20:01 +0200

    hola

--- routes/web.php
+++ routes/web.php
@@ -9,3 +9,11 @@ Route::get('/', function () {
 
 Route::get('auth/google', [LoginController::class, 'redirectToGoogle']);
 Route::get('auth/google/callback', [LoginController::class, 'handleGoogleCallback']);
+
+Route::get('/solicitudPrestamo', function () {
+    if (\Illuminate\Support\Facades\Auth::check()) {
+        return 'solicitud Prestamo';
+    } else {
+        return 'No estas logeado';
+    }
+});
