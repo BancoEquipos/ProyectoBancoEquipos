c885519a630b61820cfee931a4ae443a58492fa2
Borja <borjaab1997@gmail.com>
2021-06-05 20:04:23 +0200

    hola

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -14,31 +14,31 @@ class LoginController extends Controller
 
     public function handleGoogleCallback()
     {
-            $user = Socialite::driver('google')->stateless()->user();
-            if (!strpos($user->getEmail(),'alu.murciaeduca.es') || !strpos($user->getEmail(),'murciaeduca.es')) {
-                return view('peopleNotAutorithed');
-            }
+        $user = Socialite::driver('google')->stateless()->user();
+        if (!strpos($user->getEmail(),'alu.murciaeduca.es') || !strpos($user->getEmail(),'murciaeduca.es')) {
+            return view('peopleNotAutorithed');
+        }
 
-            $finduser = User::where('google_id', $user->id)->first();
-            if (!$finduser) {
-                $formatDate = "Y-m-d H:i:s";
-                $newUser = User::create([
-                    'name' => $user->name,
-                    'email' => $user->email,
-                    'google_id'=> $user->id,
-                    'lastLog' => date($formatDate),
-                ]);
-                $newUser->save();
-            } else {
-                $formatDate = "Y-m-d H:i:s";
-                $finduser->lastLog = date($formatDate);
-                $finduser->save();
-            }
-            if (strpos($user->getEmail(),'alu.murciaeduca.es')) {
-                $nre = explode("@", $user['email']);
-                return view('alumno', array('userName' => $user['family_name'], 'surnames' => $user['given_name'], 'nre' => $nre[0]));
-            } else {
-                return view('profesor', array('user' => $user));
-            }
+        $finduser = User::where('google_id', $user->id)->first();
+        if (!$finduser) {
+            $formatDate = "Y-m-d H:i:s";
+            $newUser = User::create([
+                'name' => $user->name,
+                'email' => $user->email,
+                'google_id'=> $user->id,
+                'lastLog' => date($formatDate),
+            ]);
+            $newUser->save();
+        } else {
+            $formatDate = "Y-m-d H:i:s";
+            $finduser->lastLog = date($formatDate);
+            $finduser->save();
+        }
+        if (strpos($user->getEmail(),'alu.murciaeduca.es')) {
+            $nre = explode("@", $user['email']);
+            return view('vistaAlumno', array('userName' => $user['family_name'], 'surnames' => $user['given_name'], 'nre' => $nre[0]));
+        } else {
+            return view('vistaProfesor', array('user' => $user));
+        }
     }
 }
