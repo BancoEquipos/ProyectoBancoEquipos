219fb95b3c68e6ac32621a8190418a3bc8d85217
Borja <borjaab1997@gmail.com>
2021-06-09 16:40:07 +0200

    Prueba vista alumno

---
+++ public/alumno/js/eventosDiv.js
@@ -0,0 +1,12 @@
+$(document).ready(function () {
+
+    function redirigir(url){
+        $(location).attr('href', url)
+    }
+
+
+    $("#servicioIncidencia").click(function e(){redirigir("https://www.google.com")});
+    $("#botonLogout").click(function e(){redirigir("https://www.google.com")});
+    $("#botonCambiar").click(function e(){redirigir("https://www.google.com")});
+    
+});
