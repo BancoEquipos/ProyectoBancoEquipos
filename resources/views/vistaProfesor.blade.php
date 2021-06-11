60f9cbd00973ad58c4198712b4eab49cd31e0f4f
Borja <borjaab1997@gmail.com>
2021-06-11 12:42:30 +0200

    a

--- resources/views/vistaProfesor.blade.php
+++ resources/views/vistaProfesor.blade.php
@@ -11,8 +11,8 @@
           integrity="sha384-B0vP5xmATw1+K9KRQjQERJvTumQW0nPEzvF6L/Z6nronJ3oUOFUFpCjEUQouq2+l" crossorigin="anonymous">
     <script src="https://code.jquery.com/jquery-3.6.0.js"
             integrity="sha256-H+K7U5CnXl1h5ywQfKtSj8PCmoN9aaq30gDh27Xc0jk=" crossorigin="anonymous"></script>
-    <link rel="stylesheet" href="estiloProfesor.css">
-    <script src="eventosDivProfesor.js"></script>
+    <link rel="stylesheet" href="/estiloProfesor.css">
+    <script src="/eventosDivProfesor.js"></script>
     <title>Seccion Profesor</title>
     <link rel="icon" href="Imagenes/logoC3Mobile.svg">
 </head>
