5c62fcb1b1e7f8c219820c2e7f90d0120a9b27fe
Borja <borjaab1997@gmail.com>
2021-06-11 11:58:41 +0200

    a

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -1,41 +1,49 @@
 <!doctype html>
 <html lang="es">
-  <head>
+
+<head>
+    <!-- Required meta tags -->
     <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-+0n0xVW2eSR5OomGNYDnhzAbDsOXxcvSN1TPprVMTNDbiYZCxYbOOl7+AMvyTG2x" crossorigin="anonymous">
-    <link href="/signin/style.css" rel="stylesheet">
-      @laravelPWA
-  </head>
-    <body class="text-center">
-        <main class="form-signin">
-            <form>
-                <img class="mb-4" src="https://www.tailorbrands.com/wp-content/uploads/2020/07/mcdonalds-logo.jpg" alt="" width="72" height="57">
-                <h1 class="h3 mb-3 fw-normal">Iniciar sesi&oacute;n</h1>
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
-                <div class="form-floating">
-                    <input type="email" class="form-control" id="floatingInput" placeholder="name@example.com">
-                    <label for="floatingInput">Correo Electronico</label>
-                </div>
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/css/bootstrap.min.css"
+          integrity="sha384-B0vP5xmATw1+K9KRQjQERJvTumQW0nPEzvF6L/Z6nronJ3oUOFUFpCjEUQouq2+l" crossorigin="anonymous">
+    <script src="https://code.jquery.com/jquery-3.6.0.js"
+            integrity="sha256-H+K7U5CnXl1h5ywQfKtSj8PCmoN9aaq30gDh27Xc0jk=" crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="estilo.css">
+    <script src="eventosDiv.js"></script>
+    <title>C3App Login</title>
+    <link rel="icon" href="Imagenes/logoC3Mobile.svg">
+</head>
 
-                <div class="form-floating">
-                    <input type="password" class="form-control" id="floatingPassword" placeholder="contraseña">
-                    <label for="floatingPassword">Contraseña</label>
-                </div>
+<body>
+<div class="container">
+    <div class="row">
+        <div class="col-12 logoDiv"><img src="Imagenes/logoC3Mobile.svg" class="imagenLogoMobile"></div>
 
-                <div class="checkbox mb-3">
-                    <label>
-                        <input type="checkbox" value="remember-me"> Recordarme
-                    </label>
-                </div>
-                <a href="/print" class="btn btn-secondary">print</a>
-                <a href="/email" class="btn btn-secondary">email</a>
-                <a href="{{ url('auth/google') }}" style="margin-top: 0px !important;background: green;color: #ffffff;padding: 5px;border-radius:7px;" class="ml-2 btn-google">
-                    <strong>Login With Google</strong>
-                </a>
-                <p class="mt-5 mb-3 text-muted">&copy; 2021–2021</p>
-            </form>
-        </main>
-        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-gtEjrD/SeCtmISkJkNUaaKMoLD0//ElJ19smozuHV6z3Iehds+3Ulb9Bn9Plx0x4" crossorigin="anonymous"></script>
-    </body>
+        <div class="row text-center contenedorHeader">
+            <div class="col-12 textoSeccion">
+                Bienvenido a C3App. Para poder acceder a los servicios que ofrecemos, por favor, inicia sesion con
+                tu cuenta de estudiante o profesor
+                (correos murciaedcua). </br>
+                Tan solo se podra acceder a la aplicacion mediante un correo de dicho tipo.
+            </div>
+        </div>
+        <div class="col-12 text-center botonDiv">
+            <button class=" btn btn-primary botonLogin" type="submit" id="botonLogin">Login
+                mediante Google</button>
+        </div>
+    </div>
+</div>
+
+<script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"
+        integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj"
+        crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-Piv4xVNRyMGpqkS2by6br4gNJ7DXjqk09RmUpJ8jgGtD7zP9yug3goQfGII0yAns"
+        crossorigin="anonymous"></script>
+
+</body>
+
 </html>
