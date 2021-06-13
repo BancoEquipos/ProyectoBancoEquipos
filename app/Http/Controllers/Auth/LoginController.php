e657398d5275800302bee61bced9689d5087c003
Borja <borjaab1997@gmail.com>
2021-06-13 23:23:52 +0200

    a

---
+++ app/Http/Controllers/Auth/LoginController.php
@@ -0,0 +1,47 @@
+<?php
+namespace App\Http\Controllers\Auth;
+use App\Http\Controllers\Controller;
+use Laravel\Socialite\Facades\Socialite;
+use App\Models\User;
+use \Illuminate\Support\Facades\Auth;
+
+class LoginController extends Controller
+{
+
+    public function redirectToGoogle()
+    {
+        return Socialite::driver('google')->with(["prompt" => "select_account"])->redirect();
+    }
+
+    public function handleGoogleCallback()
+    {
+        $user = Socialite::driver('google')->stateless()->user();
+        if (!strpos($user->getEmail(),'alu.murciaeduca.es') || !strpos($user->getEmail(),'murciaeduca.es')) {
+            return view('peopleNotAutorithed');
+        }
+
+        $finduser = User::where('google_id', $user->id)->first();
+        if (!$finduser) {
+            $formatDate = "Y-m-d H:i:s";
+            $newUser = User::create([
+                'name' => $user->name,
+                'email' => $user->email,
+                'google_id'=> $user->id,
+                'lastLog' => date($formatDate),
+            ]);
+            $newUser->save();
+            Auth::login($newUser);
+        } else {
+            $formatDate = "Y-m-d H:i:s";
+            $finduser->lastLog = date($formatDate);
+            $finduser->save();
+            Auth::login($finduser);
+        }
+        if (strpos($user->getEmail(),'alu.murciaeduca.es')) {
+            $nre = explode("@", $user['email']);
+            return view('vistaAlumno', array('userName' => $user['family_name'], 'surnames' => $user['given_name'], 'nre' => $nre[0]));
+        } else {
+            return view('vistaProfesor', array('user' => $user));
+        }
+    }
+}
