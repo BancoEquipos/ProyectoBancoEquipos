a7a88598af2ecc4189084e615f4d03492567918c
Borja <borjaab1997@gmail.com>
2021-06-14 00:00:54 +0200

    a

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -18,7 +18,6 @@ class CreateUsersTable extends Migration
             $table->string('name');
             $table->string('email')->unique();
             $table->string('google_id');
-            $table->dateTime('lastLog');
             $table->timestamps();
         });
     }
