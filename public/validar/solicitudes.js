7cca6bd6d3eea340b7d0811b5ce9c5ac4856c148
Borja <borjaab1997@gmail.com>
2021-06-16 20:41:02 +0200

    a

--- public/validar/solicitudes.js
+++ public/validar/solicitudes.js
@@ -377,7 +377,7 @@ $(document).ready(function () {
         }
 
         response = await peticion(URL_API + 'api/prestamoscomponentesespecificos/' + idPrestamo, configuracionPeticion);
-        await sleep(500);
+        // await sleep(500);
         location.reload();
         return response;
     }
@@ -421,7 +421,7 @@ $(document).ready(function () {
         }
 
         componentesDisponibles = await peticion(URL_API + 'api/prestamos/' + this.id, configuracionPeticion);
-        await sleep(500);
+        // await sleep(500);
         return componentesDisponibles;
     }
 
